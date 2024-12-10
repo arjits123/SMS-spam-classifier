cd3342a162397f5d96f772e064129ead8a6aa8de
arjits123 <arjitpkt96@gmail.com>
2024-12-10 00:15:54 +0000

    project completed

--- src/components/model_development.py
+++ src/components/model_development.py
@@ -28,69 +28,87 @@ class ModelTrainer:
 
     def initiate_model_trainer(self,X_train, X_test, y_train, y_test):
         try:
-            svc = SVC(kernel='sigmoid', gamma=1.0)
-            knc = KNeighborsClassifier()
             mnb = MultinomialNB()
-            dtc = DecisionTreeClassifier(max_depth=5)
-            lrc = LogisticRegression(solver='liblinear', penalty='l1')
-            rfc = RandomForestClassifier(n_estimators=50, random_state=2)
-            abc = AdaBoostClassifier(n_estimators=50, random_state=2)
-            etc = ExtraTreesClassifier(n_estimators=50, random_state=2)
-            gbdt = GradientBoostingClassifier(n_estimators=50,random_state=2)
-
-            models  = {
-                'SVC' : svc,
-                'KN' : knc, 
-                'NB': mnb, 
-                'DT': dtc, 
-                'LR': lrc, 
-                'RF': rfc, 
-                'AdaBoost': abc,
-                'ExtraClass': etc,
-                "GradientBoosting": gbdt
-            }
-
-            # Model Training
-            model_report = {}
-            for i in range(len(models)):
-                model = list(models.values())[i]
-                model.fit(X_train,y_train)
-                Y_pred = model.predict(X_test)
-                acc_score = accuracy_score(y_test, Y_pred)
-                prec_score = precision_score(y_test, Y_pred)
-
-                model_report[list(models.keys())[i]] = acc_score, prec_score
-            
-            logging.info('model training complete')
-
-            # Best model score
-            best_model_score = max(sorted(model_report.values()))
-
-            #Best model name - gives the key of dictionary
-            model_keys_list = list(model_report.keys())
-            model_values_list = list(model_report.values())
-            index_of_best_model = model_values_list.index(best_model_score) # lets say 2
-            best_model_name = model_keys_list[index_of_best_model]
-
-             # best model - gives the value of dictionary
-            best_model = models[best_model_name]
-            logging.info('Best model name declared')
+            mnb.fit(X_train, y_train)
+            y_pred = mnb.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            prec_score = precision_score(y_test, y_pred)
 
             save_obj(
                 file_path = self.model_trainer_config.model_trainer_path,
-                obj = best_model
+                obj = mnb
             )
-            logging.info('Best model saved')
+            return accuracy,prec_score
 
-            #Prediction of the best model
-            ypred = best_model.predict(X_test)
-            accuracy = accuracy_score(y_test, ypred)
-            precision = precision_score(y_test, ypred)
-            logging.info('best model name and score printed')
-
-
-            return best_model_name, accuracy, precision
-        
         except Exception as e:
-            raise CustomException(e,sys)
+            raise(CustomException(e,sys))
+
+
+    # def initiate_model_trainer(self,X_train, X_test, y_train, y_test):
+    #     try:
+    #         svc = SVC(kernel='sigmoid', gamma=1.0)
+    #         knc = KNeighborsClassifier()
+    #         mnb = MultinomialNB()
+    #         dtc = DecisionTreeClassifier(max_depth=5)
+    #         lrc = LogisticRegression(solver='liblinear', penalty='l1')
+    #         rfc = RandomForestClassifier(n_estimators=50, random_state=2)
+    #         abc = AdaBoostClassifier(n_estimators=50, random_state=2)
+    #         etc = ExtraTreesClassifier(n_estimators=50, random_state=2)
+    #         gbdt = GradientBoostingClassifier(n_estimators=50,random_state=2)
+
+    #         models  = {
+    #             'SVC' : svc,
+    #             'KN' : knc, 
+    #             'NB': mnb, 
+    #             'DT': dtc, 
+    #             'LR': lrc, 
+    #             'RF': rfc, 
+    #             'AdaBoost': abc,
+    #             'ExtraClass': etc,
+    #             "GradientBoosting": gbdt
+    #         }
+
+    #         # Model Training
+    #         model_report = {}
+    #         for i in range(len(models)):
+    #             model = list(models.values())[i]
+    #             model.fit(X_train,y_train)
+    #             Y_pred = model.predict(X_test)
+    #             acc_score = accuracy_score(y_test, Y_pred)
+    #             prec_score = precision_score(y_test, Y_pred)
+
+    #             model_report[list(models.keys())[i]] = acc_score, prec_score
+            
+    #         logging.info('model training complete')
+
+    #         # Best model score
+    #         best_model_score = max(sorted(model_report.values()))
+
+    #         #Best model name - gives the key of dictionary
+    #         model_keys_list = list(model_report.keys())
+    #         model_values_list = list(model_report.values())
+    #         index_of_best_model = model_values_list.index(best_model_score) # lets say 2
+    #         best_model_name = model_keys_list[index_of_best_model]
+
+    #          # best model - gives the value of dictionary
+    #         best_model = models[best_model_name]
+    #         logging.info('Best model name declared')
+
+    #         save_obj(
+    #             file_path = self.model_trainer_config.model_trainer_path,
+    #             obj = best_model
+    #         )
+    #         logging.info('Best model saved')
+
+    #         #Prediction of the best model
+    #         ypred = best_model.predict(X_test)
+    #         accuracy = accuracy_score(y_test, ypred)
+    #         precision = precision_score(y_test, ypred)
+    #         logging.info('best model name and score printed')
+
+
+    #         return best_model_name, accuracy, precision
+        
+    #     except Exception as e:
+    #         raise CustomException(e,sys)
         
